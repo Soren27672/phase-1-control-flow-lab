8808ea46281ec1655ad737f591ec6b4e88e752b0
Maxwell Gottesman <maxgman16@yahoo.com>
2022-10-12 16:04:06 -0400

    commit tree da542bd62edd3b178324ff9daca3533ec99b1dc7

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(length){
+  if (length <= 400) return 'This one is on me!';
+  else if (length > 400 && length < 2000) return 'That will be twenty bucks.';
+  else if (length < 2500) return 'I will gladly take your thirty bucks.';
+  else return 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
